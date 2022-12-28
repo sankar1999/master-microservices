4dab57ebb1537baaefb26f9a0bbfa00d7025d498
sankar1999 <sankarsathya1973@gmail.com>
2022-12-29 01:28:00 +0530

    Docker Completed

--- currency-exchange-service/src/main/java/com/currencyexchange/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/currencyexchange/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.currencyexchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,10 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+//    @Autowired
+    private Logger logger = LoggerFactory
+            .getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -21,6 +27,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue value called with {} to {}", from, to);
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
